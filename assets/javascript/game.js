4befeb11168ad3c482bb0a27856665b92f371644
Mac User <alexarobinson19@gmail.com>
2017-11-08 00:28:05 -0500

    Fixed function issues for dashes function and guessLetter with help from TA Jen. Now I can get the letters to show up with a hardcoded word. I need to add the randomization for my word array, add an array with the word choices, add in my counter variables, display the array of letters, and figure out how to get the code to repeat after someone wins.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ var win=0;
 var tries=15;
 var triedLetters=[];
 var alphabet=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o","p", "q","r","s","t","u","v","w","x","y","z"];
+var dashArray=[];    
 
 
 //add function for the randomization, return the variable word.
@@ -14,19 +15,28 @@ function wordToArray(aWord){
     return aWord.split("");
 }
 
+
 //variable storing the letters of the chosen word
 var letterArray= wordToArray (word);
 console.log(letterArray);
 
-//not sure why this isn't working
+//function for printing out dashes and letters
+function replaceDash(){
+    document.getElementById("currentWord").innerHTML = dashArray.join(" ");
+        
+    }
+
+//declared function for creating dashes
 function dashes(){ 
-    for(var i=0; i<=letterArray.length; i++){
-    document.getElementById("currentWord").innerHTML(dashArray.join(" "));
-        var dashArray=[];
+    for(var i=0; i<letterArray.length; i++){
         dashArray.push("__")
     }
-    //need help with this for sure
-    document.getElementById("currentWord").innerHTML(dashArray.join(" "));
+    replaceDash();
+    // document.getElementById("currentWord").innerHTML = dashArray.join(" ");
+    }
+
+    function displayLetters(){
+        
     }
 
 //when a key is pressed 
@@ -37,21 +47,27 @@ document.onkeyup = function(event) {
     //making sure the letter is lowercase for comparison
     letter.toLowerCase();
     //the key is checked to see if it is part of the alphabet.
-    if(alphabet.indexof(letter)>=0){
+    if(alphabet.indexOf(letter)>=0){
         //then the key is checked to see if it is a unique letter.
-        if(triedLetters.indexof(letter)===-1){
+        if(triedLetters.indexOf(letter)===-1){
             //a new letter is added to the array of attempted letters.
             triedLetters.push(letter);
-            //insert the check function here.
+            displayLetters();
+            //check letter input against letterArray.
             for(var j=0; j<letterArray.length; j++){
                 if(letterArray[j]===letter){
+                    //when a letter matches a certain letter in the letterArray
+                    //the letter will replace the equivalent spot in the dashArray.
                     dashArray[j]=letter;
+                    console.log(dashArray[j]);
                     //however I am replacing the dash with the letter. 
-                    //Does the method I am using now work?
                 }
+            replaceDash();
             } 
         }     
-            
+       else{
+           alert("Enter a new letter!")
+       }     
     }
     else{
       alert("Enter a letter!");
@@ -60,6 +76,7 @@ document.onkeyup = function(event) {
 }
 
 dashes();
+guessLetter();
 
 
 
